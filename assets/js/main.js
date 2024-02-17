8ce75acb013c7527b5cde365a714703df210f412
giuliano-amorim <giuliano_lopes72@hotmail.com>
2024-02-17 18:55:00 -0300

    button abilities in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,32 @@ const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
 const maxRecords = 151
-const limit = 10
+const limit = 15
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+        <button class="card ${pokemon.name}" type="button">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+                
+                <div class="hiddenDetails">
+                        <h5>Abilities</h5>
+                        <ul class="abilities">
+                             ${pokemon.abilities.map((ability) => `<li class="ability ${ability}">${ability}</li>`).join('')}
+                        </ul>
+                </div>
+            </li>
+        </button>
     `
 }
 
